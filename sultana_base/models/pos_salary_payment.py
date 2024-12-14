7dafaf171febd31311c94452e5da586c6c9a0c27
Haithem Bouhelais <haithem.bhls@gmail.com>
2024-12-14 17:39:24 +0100

    salary patch

--- sultana_base/models/pos_salary_payment.py
+++ sultana_base/models/pos_salary_payment.py
@@ -10,3 +10,12 @@ class POSSalaryPayment(models.Model):
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id)
     amount = fields.Monetary(currency_field='currency_id')
     date = fields.Date(default=fields.Date.today)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if not orderby and groupby:
+            orderby_list = [groupby] if isinstance(groupby, str) else groupby
+            orderby_list = [field.split(':')[0] for field in orderby_list]
+            orderby = ','.join([f"{field} desc" if field == 'date' else field for field in orderby_list])
+        return super().read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
+
